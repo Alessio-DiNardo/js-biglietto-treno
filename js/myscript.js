ac62d0d9d99286a02fc5e807f6744df6679838b0
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-12 15:19:09 +0200

    start try the element

---
+++ js/myscript.js
@@ -0,0 +1,19 @@
+//dichiaro una variabile CONST per l'età del passeggero
+// dichiaro una seconda variabile CONST per il numero di kilometri
+
+let peopleYoung = prompt("|Insert your age");
+let peopleOld = prompt("|Insert your age");
+
+const underAge = 20%;
+
+const overAge = 40%;
+
+const userAge = parseInt(prompt("Insert your age"));
+
+const currentYear = 2023;
+
+let yearOfBirth = currentYear - userAge;
+
+console.log(yearOfBirth)
+
+
