a547f8e49dbf8a2f56e17073376fa01e804d75ed
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-12 15:51:22 +0200

    add kilometers

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,6 @@
 //dichiaro una variabile CONST per l'età del passeggero
 // dichiaro una seconda variabile CONST per il numero di kilometri
 
-let firstAge = prompt("Insert your age");
-const discount = prompt("65");
-
 const userAge = parseInt(prompt("Insert your age"));
 
 const currentYear = 2023;
@@ -12,13 +9,15 @@ let yearOfBirth = currentYear - userAge;
 
 console.log(yearOfBirth);
 
+const kilometers = prompt("insert your kilometers")
+
 //const underAge = 20%;
 
 //const overAge = 40%;
 
 
 if (firstAge < discount) {
-        console.log("avra il 20% di sconto");
+        console.log("avrà il 20% di sconto");
 } else if (firstAge > discount){
         console.log("avrà il 40% di sconto");
 }
