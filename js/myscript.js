1145e3bafa0e6d26c4bae39bc8666ba51c3e45f1
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-12 16:30:42 +0200

    add if and else if

--- js/myscript.js
+++ js/myscript.js
@@ -11,19 +11,16 @@ console.log(yearOfBirth);
 
 const distance = parseInt(prompt("insert your kilometers"))
 
-const kilometers = 0.21;
+const priceForKm = 0.21;
 
-let price = kilometers * distance;
+
+let price = priceForKm * distance;
 
 console.log(price);
 
-//const underAge = 20%;
 
-//const overAge = 40%;
-
-
-if (firstAge < discount) {
-        console.log("avrà il 20% di sconto");
-} else if (firstAge > discount){
-        console.log("avrà il 40% di sconto");
+if (userAge < 18) {
+        console.log((20 / 100) * price.toFixed);
+} else if (firstAge > 65){
+        console.log((40 / 100) * price.toFixed);
 }
